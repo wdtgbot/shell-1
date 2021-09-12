d97c1981c39dfae7b3a9049fc2673000179794eb
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 21:51:00 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -5,9 +5,11 @@ echo '啊'
 #sh <(curl -fsSL https://get.docker.com)
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-
+echo '这'
 y | bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+echo '错'
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
+echo '了'
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
