80c123f1acf8926228dc881e02a7c581fa8d0521
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 21:38:24 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -5,7 +5,7 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" 
 #sh <(curl -fsSL https://get.docker.com)
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-chsh -s /bin/zsh
+
 bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) y
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -13,3 +13,4 @@ git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i "s/plugins=(git/plugins=(git zsh-autosuggestions git-open zsh-syntax-highlighting/g" ~/.zshrc
 source ~/.zshrc
+chsh -s /bin/zsh
