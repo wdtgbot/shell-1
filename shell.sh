f0e860391d2c4dd60ef5e322ed19e37b8e52cac9
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 21:36:45 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt install curl wget zsh git sudo -y
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone	
-chsh -s /bin/zsh
+
 sh <(curl -fsSL https://get.docker.com)
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-
+chsh -s /bin/zsh
 bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
