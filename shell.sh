2cd6819f451af2bd54404973b460c6d7b19980ff
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 21:37:03 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,7 +6,7 @@ sh <(curl -fsSL https://get.docker.com)
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 chsh -s /bin/zsh
-bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) y
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
