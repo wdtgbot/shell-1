c20a7a4de6b4d8d12e5640b87456cb11107769c9
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 22:43:39 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -10,11 +10,11 @@ chsh -s /bin/zsh
 echo '错'
 #bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
 
-sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
-echo '了'
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i "s/plugins=(git/plugins=(git zsh-autosuggestions git-open zsh-syntax-highlighting/g" ~/.zshrc
-source ~/.zshrc
+#sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
+#echo '了'
+#git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#sed -i "s/plugins=(git/plugins=(git zsh-autosuggestions git-open zsh-syntax-highlighting/g" ~/.zshrc
+#source ~/.zshrc
 
