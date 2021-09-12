5c80ac87b4167eff377571594187d5deab7552cd
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-12 22:40:16 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,8 +6,10 @@ echo '啊'
 #echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 #echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 echo '这'
-sh <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) y
+chsh -s /bin/zsh
 echo '错'
+bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
 echo '了'
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -15,4 +17,4 @@ git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i "s/plugins=(git/plugins=(git zsh-autosuggestions git-open zsh-syntax-highlighting/g" ~/.zshrc
 source ~/.zshrc
-chsh -s /bin/zsh
+
