025956f016e24703a2d2985228e93e76c0effc1e
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-09-13 20:00:55 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,16 +6,13 @@ sh <(curl -fsSL https://get.docker.com)
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 
-#bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
-#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-#sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
+
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 curl https://raw.githubusercontent.com/Ecalose/shell/main/.zshrc -o ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#sed -i "s/plugins=(git/plugins=(git zsh-autosuggestions git-open zsh-syntax-highlighting extract/g" ~/.zshrc
+
 source ~/.zshrc
 chsh -s /bin/zsh
