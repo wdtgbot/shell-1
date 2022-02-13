f3c7056d6023541a358452917ed18b35dcfe1d63
Ecalose <47114714+Ecalose@users.noreply.github.com>
2022-02-13 14:56:16 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+bash <(curl -fsSL https://get.docker.com) &
+wait
+
+mkdir ss
+cat << EOF > /root/ss/config.json
+{
+"server": "0.0.0.0",
+"server_port": 8388,
+"password": "99b5a05f-43a1-4138-96b2-4c6d03052253",
+"method": "chacha20-ietf-poly1305"
+}
+EOF
+
 docker run --name ss \
 --restart always \
 -p 39723:8388/tcp \
